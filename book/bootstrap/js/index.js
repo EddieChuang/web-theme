51d7a8a94e5286cfccd45309579d976b69b06c2e
EddieChuang <az564123@gmail.com>
2018-08-26 12:49:35 +0800

    done

--- book/bootstrap/js/index.js
+++ book/bootstrap/js/index.js
@@ -1,3 +1,22 @@
 $(function() {
   $('#year').text(new Date().getFullYear())
+
+  $('body').scrollspy({ target: '#main-nav' })
+
+  $('#main-nav a').on('click', function(e) {
+    e.preventDefault()
+
+    const hash = this.hash
+    if (hash !== '') {
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        () => {
+          window.location.hash = hash
+        }
+      )
+    }
+  })
 })
